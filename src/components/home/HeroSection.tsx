85c3626949ff78eac92e5221e2398ac4912ad236
SamadRahmKhan <samadrahim47@gmail.com>
2026-01-23 12:53:55 +0500

    HOMe page

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -71,11 +71,11 @@ export function HeroSection() {
       </div>
 
       {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 z-10 animate-bounce">
+      {/* <div className="absolute bottom-8 left-1/2 -translate-x-1/2 z-10 animate-bounce">
         <div className="w-6 h-10 rounded-full border-2 border-primary-foreground/30 flex items-start justify-center p-2">
           <div className="w-1 h-2 rounded-full bg-primary-foreground/50" />
         </div>
-      </div>
+      </div> */}
     </section>
   );
 }
